a81c21914652fb69a73b04e115c7d427bc06fbdc
unknown <anonuevoloraine6@gmail.com>
2023-07-27 13:58:00 +0800

    add labels and entry widget

--- GUI.py
+++ GUI.py
@@ -15,3 +15,28 @@ class ContactTracingAppGUI:
         main_frame = ttk.Frame(root, style='Medical.TFrame')
         main_frame.pack(padx=20, pady=20)
 
+# Labels and Entry widgets for input
+        name_label = tk.Label(main_frame, text="Full Name:", font=font_style)
+        name_label.grid(row=0, column=0, pady=10)
+        self.name_var = tk.StringVar()
+        name_entry = tk.Entry(main_frame, textvariable=self.name_var, font=font_style)
+        name_entry.grid(row=0, column=1, pady=10)
+
+        phone_label = tk.Label(main_frame, text="Phone Number:", font=font_style)
+        phone_label.grid(row=1, column=0, pady=10)
+        self.phone_var = tk.StringVar()
+        phone_entry = tk.Entry(main_frame, textvariable=self.phone_var, font=font_style)
+        phone_entry.grid(row=1, column=1, pady=10)
+
+        date_label = tk.Label(main_frame, text="Date of Contact (YYYY-MM-DD):", font=font_style)
+        date_label.grid(row=2, column=0, pady=10)
+        self.date_var = tk.StringVar()
+        date_entry = tk.Entry(main_frame, textvariable=self.date_var, font=font_style)
+        date_entry.grid(row=2, column=1, pady=10)
+
+        location_label = tk.Label(main_frame, text="Location of Contact:", font=font_style)
+        location_label.grid(row=3, column=0, pady=10)
+        self.location_var = tk.StringVar()
+        location_entry = tk.Entry(main_frame, textvariable=self.location_var, font=font_style)
+        location_entry.grid(row=3, column=1, pady=10)
+
