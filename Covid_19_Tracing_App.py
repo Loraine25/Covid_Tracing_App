991ca7aaa2f24fd987274ef661f760ea6ec0d4eb
unknown <anonuevoloraine6@gmail.com>
2023-07-27 13:53:22 +0800

    add function to search

--- Covid_19_Tracing_App.py
+++ Covid_19_Tracing_App.py
@@ -15,3 +15,25 @@ def add_entry():
         writer.writerow([name, phone, date, location])
     
     print("Entry added successfully!")
+
+# Function to search for entries in the contact tracing app
+def search_entry():
+    search_term = input("Enter the name or phone number to search: ")
+    
+    with open("contact_tracing.csv", "r") as file:
+        reader = csv.reader(file)
+        found_entries = []
+        for row in reader:
+            if search_term.lower() in [item.lower() for item in row]:
+                found_entries.append(row)
+    
+    if found_entries:
+        print("Found entries:")
+        for entry in found_entries:
+            print("Name:", entry[0])
+            print("Phone:", entry[1])
+            print("Date of contact:", entry[2])
+            print("Location of contact:", entry[3])
+            print("--------------------")
+    else:
+        print("No entries found.")
